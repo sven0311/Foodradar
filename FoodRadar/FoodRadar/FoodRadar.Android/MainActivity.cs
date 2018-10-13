68bb1a3d33100fb5cbf315b03963e647feca09ff
Will Richardson-Davis <w.richardsondavis@gmail.com>
2018-10-13 21:31:46 +1000

    Locked Android Orientation

--- FoodRadar/FoodRadar/FoodRadar.Android/MainActivity.cs
+++ FoodRadar/FoodRadar/FoodRadar.Android/MainActivity.cs
@@ -15,7 +15,8 @@ using Android.Support.V4.App;
 
 namespace FoodRadar.Droid
 {
-    [Activity(Label = "FoodRadar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    [Activity(Label = "FoodRadar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
