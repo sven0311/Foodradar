0b451f355ada9bb578c1ecb2be97d1ac46883d4e
Will Richardson-Davis <w.richardsondavis@gmail.com>
2018-10-22 20:11:16 +1000

    Making back buttons look correct. img still not working

--- FoodRadar/FoodRadar/FoodRadar.Android/Resources/Resource.designer.cs
+++ FoodRadar/FoodRadar/FoodRadar.Android/Resources/Resource.designer.cs
@@ -2462,26 +2462,26 @@ namespace FoodRadar.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02014a
-			public const int avd_hide_password_1 = 2130837834;
-			
 			// aapt resource value: 0x7f02014b
-			public const int avd_hide_password_2 = 2130837835;
+			public const int avd_hide_password_1 = 2130837835;
 			
 			// aapt resource value: 0x7f02014c
-			public const int avd_hide_password_3 = 2130837836;
+			public const int avd_hide_password_2 = 2130837836;
+			
+			// aapt resource value: 0x7f02014d
+			public const int avd_hide_password_3 = 2130837837;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02014d
-			public const int avd_show_password_1 = 2130837837;
-			
 			// aapt resource value: 0x7f02014e
-			public const int avd_show_password_2 = 2130837838;
+			public const int avd_show_password_1 = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int avd_show_password_3 = 2130837839;
+			public const int avd_show_password_2 = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int avd_show_password_3 = 2130837840;
 			
 			// aapt resource value: 0x7f020055
 			public const int common_full_open_on_phone = 2130837589;
@@ -3188,11 +3188,11 @@ namespace FoodRadar.Droid
 			// aapt resource value: 0x7f02013f
 			public const int notification_icon_background = 2130837823;
 			
-			// aapt resource value: 0x7f020148
-			public const int notification_template_icon_bg = 2130837832;
-			
 			// aapt resource value: 0x7f020149
-			public const int notification_template_icon_low_bg = 2130837833;
+			public const int notification_template_icon_bg = 2130837833;
+			
+			// aapt resource value: 0x7f02014a
+			public const int notification_template_icon_low_bg = 2130837834;
 			
 			// aapt resource value: 0x7f020140
 			public const int notification_tile_bg = 2130837824;
@@ -3204,19 +3204,22 @@ namespace FoodRadar.Droid
 			public const int ProfilePic = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int round_map_white_24dp = 2130837827;
+			public const int round_arrow_back_white_24dp = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int round_person_white_24dp = 2130837828;
+			public const int round_map_white_24dp = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int round_search_white_24dp = 2130837829;
+			public const int round_person_white_24dp = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int tooltip_frame_dark = 2130837830;
+			public const int round_search_white_24dp = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int tooltip_frame_light = 2130837831;
+			public const int tooltip_frame_dark = 2130837831;
+			
+			// aapt resource value: 0x7f020148
+			public const int tooltip_frame_light = 2130837832;
 			
 			static Drawable()
 			{
